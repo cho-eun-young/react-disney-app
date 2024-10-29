48e45a080010b37c0a4d24558dd6b0b1461b7341
cho-eun-young <choeunyoung224@gmail.com>
2024-10-29 16:19:40 +0900

    241029 login

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,22 @@
 import React, { useEffect, useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { Navigate, useLocation, useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import LoginPage from "../pages/LoginPage";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import app from "../firebase";
 
 const Nav = () => {
   const [show, setShow] = useState(false);
   const [searchValue, setSearchValue] = useState("");
-  const navigation = useNavigate();
+  const navigate = useNavigate();
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth(app);
+  const [userData, setUserData] = useState(null);
 
   const { pathname } = useLocation();
 
@@ -28,25 +38,61 @@ const Nav = () => {
     });
   }, []);
 
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        navigate("/");
+      } else if (user && pathname === "/") {
+        navigate("/main");
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [auth, navigate, pathname]);
+
   const handleChange = (e) => {
     setSearchValue(e.target.value);
-    navigation(`/search?q=${e.target.value}`);
+    navigate(`/search?q=${e.target.value}`);
   };
 
   const handleAuth = () => {
     // firebase에서 제공한 방법 사용
     // google login을 위해서
+
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result);
+        setUserData({
+          id: result.user.id,
+          email: result.user.email,
+          displayName: result.user.displayName,
+          photoURL: result.user.photoURL,
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const handleLogOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUserData(null);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
   return (
     <NavWrapper $show={show}>
       <Logo>
         <img alt="Disney plus logo" src="/images/logo.svg" />
       </Logo>
-
       {/* URL --> useLocation
     다이나믹한 부분 ---> useParams 
   */}
-
       {pathname === "/" ? (
         <Login onClick={handleAuth}>Login</Login>
       ) : (
@@ -58,9 +104,54 @@ const Nav = () => {
           placeholder="영화를 검색해주세요."
         />
       )}
+      {pathname !== "/" && (
+        <SignOut>
+          <UserImg src={userData?.photoURL} alt={userData?.displayName} />
+          <DropDown onClick={handleLogOut}>
+            <span>Sign Out</span>
+          </DropDown>
+        </SignOut>
+      )}
     </NavWrapper>
   );
 };
+
+const UserImg = styled.img`
+  border-radius: 50%;
+  width: 100%;
+  height: 100%;
+`;
+
+const DropDown = styled.div`
+  position: absolute;
+  top:48px;
+  right:0px;
+  background: rgb(19,19,19)
+  border: 1px solid rgba(151,151,151,0.34);
+  border-radius: 4px;
+  padding:10px;
+  font-size:14px;
+  letter-spacing:3px;
+  width:100px;
+  opacity:0;
+`;
+
+const SignOut = styled.div`
+  position: realtive;
+  height: 48px;
+  width: 48px;
+  display: flex;
+  cusor: pointer;
+  align-items: center;
+  justify-content: center;
+
+  &:hover {
+    ${DropDown} {
+      opacity: 1;
+      transition-duration: 1s;
+    }
+  }
+`;
 
 const Login = styled.a`
   background: rgba(0, 0, 0, 0.6);
